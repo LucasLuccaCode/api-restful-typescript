c1b6a57222329f36f9e521781ca9db116c5e931e
Lucca Santos <lucasluccad.r@outlook.com>
2023-02-09 13:42:28 -0300

    feat: install dependencies and development dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+import express, { Request, Response } from 'express'
+import config from 'config'
+
+const app = express()
+const PORT = config.get<number>('port')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Home')
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
