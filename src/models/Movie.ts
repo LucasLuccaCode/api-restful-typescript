4377da801a08188d1d1af174816e9fd74f2d6d6a
Lucca Santos <lucasluccad.r@outlook.com>
2023-02-09 17:26:38 -0300

    feat: create model movie

---
+++ src/models/Movie.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose'
+
+const movieSchema = new Schema({
+  title: {
+    type: String
+  },
+  rating: {
+    type: Number
+  },
+  description: {
+    type: String
+  },
+  director: {
+    type: String
+  },
+  stars: {
+    type: Array
+  },
+  poster: {
+    type: String
+  }
+}, {
+  timestamps: true
+})
+
+export const Movie = model('Movie', movieSchema)
